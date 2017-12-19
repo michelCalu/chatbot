e9493d5935c395b9be28a53d7d0c4de461234368
julien <julien@julien-fixe-liege>
2017-12-19 21:58:38 +0100

    ajout questions itérations 1 et 2

---
+++ questions_simples1.pl
@@ -0,0 +1,34 @@
+% questions bouche
+%
+% Que donne le [vin] en bouche ?
+% Comment est le [vin] en bouche ?
+% Comment est l’attaque du [vin] ?
+% Parlez-moi de la longueur du [vin] ?
+
+% questions nez
+%
+% Quel nez présente le [vin] ?
+% Quels sont les arômes du [vin] ?
+% Quel est le bouquet du [vin] ?
+% Quelles sont les propriétés olfactives du [vin] ?
+
+% questions région
+%
+% Quelle est l’appellation du [vin] ?
+% De quelle région provient le [vin] ?
+% D’où provient le [vin] ?
+% De quelle origine est le [vin] ?
+
+% questions prix
+%
+% Quel est le prix du [vin] ?
+% Combien coûte le [vin] ?
+% Que coûte le [vin] ?
+% À quelle prix puis-je acheter le [vin] ?
+
+% questions description
+%
+% Pouvez-vous me parler du [vin] ?
+% Décrivez-moi le [vin] ?
+% Présentez-moi le [vin] ?
+% Que pouvez-vous me dire au sujet du [vin] ?
---
+++ questions_simples2.pl
@@ -0,0 +1,13 @@
+% questions région
+%
+% Que recouvre l’appellation [région] ?
+% Quels vins de [région] me conseillez-vous ?
+% Quels vins de [région] avez-vous ?
+% Auriez-vous un [région] ?
+
+% questions prix
+%
+% Auriez-vous des vins entre [prix_min] et [prix_max] ?
+% Quel est votre vin le plus cher ?
+% Quel est votre vin le moins cher ?
+% Auriez-vous des vins à moins de [prix_max] ?
