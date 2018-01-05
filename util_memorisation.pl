63c761f1078b9aa2a4bc7112525740ed0a8fe200
Julien Albert <julien.albert@student.unamur.be>
2018-01-05 18:13:02 +0100

    debug, nettoyage et spec util_memorisation et util_recherche

--- util_memorisation.pl
+++ util_memorisation.pl
@@ -1,61 +1,107 @@
-% -------------------------------------------------------------------------
-% init liste_memo avec Liste moins les 3 premiers éléments qui sont renvoyer
+/* --------------------------------------------------------------------- */
+/*                                                                       */
+/*                      UTILITAIRES MEMORISATION                         */
+/*                                                                       */
+/* --------------------------------------------------------------------- */
+
+/***************************************************************************/
+% initialiste la variable globale liste_memo avec une liste et renvoie 3 éléments
+% format :
+%        set_liste_memo([A,B,C|Rest], [A,B,C])
+%        in : [A,B,C|Rest] est une liste d'éléments (typiquement des identifiants
+%             de vins)
+%        out : [A,B,C] est une liste d'éléments
+%        effect : la liste Rest est affecté à la variable gloable liste_memo
+
+% cas ou Liste est de taille inférieur à 3
 set_liste_memo(Liste, Liste) :-
-  %reset_vin_memo(),
   length(Liste, L),
   L =< 3,
   nb_setval(liste_memo, []).
 
+% cas général
 set_liste_memo([A,B,C|Rest], [A,B,C]) :-
-  %reset_vin_memo(),
   nb_setval(liste_memo, Rest).
 
+/***************************************************************************/
 % récupère les 3 premiers éléments de la liste en mémo et met à jour
+% format :
+%         get_three_elem_memo(Liste)
+%         out : Liste est la liste des 3 premiers éléments de la liste dans la
+%               variable globale liste_memo
+%         effect : les trois premiers éléments de la liste contenue dans liste_memo
+%                  sont supprimés
+
+% cas où la taille de la liste en mémoire est inférieur à 3
 get_three_elem_memo(Liste) :-
   nb_getval(liste_memo, Liste),
   length(Liste, L),
   L =< 3,
   nb_setval(liste_memo, []).
 
+% cas général
 get_three_elem_memo([A,B,C]) :-
   nb_getval(liste_memo, [A,B,C|Rest]),
   nb_setval(liste_memo, Rest).
 
+/***************************************************************************/
+% remet la variable globale liste_memo à zéro (liste vide)
 reset_liste_memo() :-
   nb_setval(liste_memo, []).
 
-% -------------------------------------------------------------------------
-% modifie vin_memo en retirant la propriété citée
-% ID_vin = identifiant vin
-% Propriete = propriété citée (dans [bouche,nez,region,prix,description])
+/***************************************************************************/
+% modifie la variable globale vin_memo en retirant la propriété donnée
+% format :
+%         set_vin_memo(ID_Vin, Propriete_Citee)
+%         in : ID_vin est un identifiant de vin, Propriete_Citee est une propriété
+%              de ce vin (représenté par une constante)
+%         precondition : ID_Vin est égal à l'identifiant dans la variable globale
+%                        vin_memo
+%         effect : Propriete_Citee est supprimé (s'il y était) de la liste des
+%                  propriétés dans la variable globale vin_memo
+
 set_vin_memo(ID_Vin, Propriete_Citee) :-
-  %reset_liste_memo(),
   nb_getval(vin_memo, vin_prop(ID_Vin, Proprietes)),
   delete(Proprietes, Propriete_Citee, Proprietes_maj),
   nb_setval(vin_memo, vin_prop(ID_Vin, Proprietes_maj)).
 
-% modifie vin_memo en remettant la liste au départ et en retirant la propriété citée
+/***************************************************************************/
+% modifie la variable globale vin_memo en retirant la propriété donnée
+% format :
+%         set_vin_memo(ID_Vin, Propriete_Citee)
+%         in : ID_vin est un identifiant de vin, Propriete_Citee est une propriété
+%              de ce vin (représenté par une constante)
+%         precondition : ID_Vin n'est pas égal à l'identifiant dans la variable
+%                        globale vin_memo
+%         effect : la variable globale vin_memo est mise à jour avec ID_Vin
+%                  et la liste des propriétés [bouche,nez,region,prix,description]
+%                  moins Propriete_Citee (s'il y était)
+
 set_vin_memo(ID_Vin, Propriete_Citee) :-
-  %reset_liste_memo(),
   not(nb_getval(vin_memo, vin_prop(ID_Vin, _))),
   delete([bouche,nez,region,prix,description], Propriete_Citee, Proprietes),
   nb_setval(vin_memo, vin_prop(ID_Vin, Proprietes)).
 
-% récupère une propriete non citée ou une liste vide si plus rien
+/***************************************************************************/
+% récupère une propriete non citée (ou une liste vide si plus rien) dans la
+% variable globale vin_memo
 get_propriete_non_citee([Propriete]) :-
   nb_getval(vin_memo, vin_prop(_, [Propriete|_])).
 
 get_propriete_non_citee([]) :-
   nb_getval(vin_memo, vin_prop(_, [])).
 
-% récupère l'ID du vin cité
+/***************************************************************************/
+% récupère l'ID du vin dans la variable globale vin_memo
 get_vin_memo(ID_Vin) :-
   nb_getval(vin_memo, vin_prop(ID_Vin, _)).
 
+/***************************************************************************/
 % remet à zéro la variable globale vin_memo
 reset_vin_memo() :-
   nb_setval(vin_memo, vin_prop(empty,[])).
 
-% test si il y un vin cité valide
+/***************************************************************************/
+% teste si il y un vin valide dans la variable globale vin_memo
 pas_de_vin_memo() :-
   nb_getval(vin_memo, vin_prop(empty, _)).
--- util_recherche.pl
+++ util_recherche.pl
@@ -1,46 +1,70 @@
-﻿% récupère l'ID d'un vin à partir d'une liste de mots (typiquement une question)
+/* --------------------------------------------------------------------- */
+/*                                                                       */
+/*                       UTILITAIRES RECHERCHE                           */
+/*                                                                       */
+/* --------------------------------------------------------------------- */
+
+/***************************************************************************/
+% récupère l'ID d'un vin à partir d'une liste de mots (typiquement une question)
+% format :
+%        get_vin(ListeMots, IDVin)
+%        in : ListeMots est une liste de mots
+%        out : IDVin est l'identifiant d'un vin dont le nom est une sous-liste
+%              de ListeMots
+
 get_vin(L_mots, ID) :-
   sublist(Nom_Vin, L_mots),
   nom(ID, Nom_Vin).
 
+/***************************************************************************/
 % récupère le nom d'une région à partir d'une liste de mots (typiquement une question)
+% format :
+%        get_region(ListeMots, Region)
+%        in : ListeMots est une liste de mots
+%        out : Region est le nom d'une région qui est un élément de ListeMots
+
 get_region(L_mots, Region) :-
   sublist([Region], L_mots),
   region(_, Region).
 
+/***************************************************************************/
 % récupère une liste des vins d'une région donnée
+% format :
+%        get_vins_de_region(Region, ListeIDvins)
+%        in : Region est le nom d'une région
+%        out : ListeIDvins est la liste des identifiants de vins appartenant à
+%              cette région
+
 get_vins_de_region(Region,  L) :-
      findall( ID, region(ID, Region), L).
 
-% récupère une liste d'ID à partir d'une liste de noms de vins
-get_ids_noms([], []).
-get_ids_noms([ID|T], [Nom|Rest]) :-
-      nom(ID,Nom),
-      get_ids_noms(T,Rest).
-
-
-
-
 /***************************************************************************/
-% vins_moins_de_max(Max, Lvins)
-%     out: Lvins = liste des vins dont le prix est <= Max
-%
-vins_moins_de_max(Max,Lvins) :-
-      findall( (Vin) , prix_moins_de_max(Vin,P,Max), Lvins ).
+% récupère une liste de vins dont le prix est inférieur à un certain montant
+% format :
+%        vins_moins_de_max(Max, Lvins)
+%        in : Max est le montant maximum
+%        out : Lvins est la liste des identifiants des vins dont le prix est <= Max
 
-prix_moins_de_max(Vin,P,Max) :-
-      prix(Vin,P),
+vins_moins_de_max(Max,Lvins) :-
+      findall( (IDVin) , prix_moins_de_max(IDVin,Max), Lvins ).
+
+% est vrai si le prix de IDVin est inférieur ou égal à Max
+prix_moins_de_max(IDVin,Max) :-
+      prix(IDVin,P),
       P =< Max.
 
 /***************************************************************************/
-% prix_plus_de_min(Min, Lvins)
-%     out: Lvins = liste des vins dont le prix est >= Min
-%
+% récupère une liste de vins dont le prix est supérieur à un certain montant
+% format :
+%        prix_plus_de_min(Min, Lvins)
+%        in : Min est le montant minimum
+%        out : Lvins est la liste des identifiants des vins dont le prix est >= Min
 
 vins_plus_de_min(Min,Lvins) :-
-      findall( (Vin) , prix_plus_de_min(Vin,P,Min), Lvins ).
+      findall( (IDVin) , prix_plus_de_min(IDVin,Min), Lvins ).
 
-prix_plus_de_min(Vin,P,Min) :-
+% est vrai si le prix de IDVin est supérieur ou égal à Min
+prix_plus_de_min(Vin,Min) :-
       prix(Vin,P),
       P >= Min.
 
@@ -59,28 +83,49 @@ vin_prix_min(X):- prix(X, _), not( moins_cher(X)), !.
 moins_cher(X):- prix(X, PrixX), prix(Y, PrixY), Y\=X, PrixX > PrixY.
 
 /***************************************************************************/
-% vins_prix_min_max(Min, Max, Result)
-%     out: Liste de tous les vins de Min<=Prix<=Max
-%
-vins_prix_min_max(Min,Max,Lvins) :-
-      findall( (Vin) , prix_vin_min_max(Vin,P,Min,Max), Lvins ).
+% récupère une liste de vins dont le prix est dans une fourchette donnée
+% format :
+%        vins_prix_min_max(Min,Max,Lvins)
+%        in : Min est le montant minimum et Max est le montant maximum
+%        out : Lvins est la liste des identifiants des vins dont le prix est >= Min
+%              et <= Max
 
-prix_vin_min_max(Vin,P,Min,Max) :-
+vins_prix_min_max(Min,Max,Lvins) :-
+      findall( (Vin) , prix_vin_min_max(Vin,Min,Max), Lvins ).
+
+% est vrai si le prix de IDVin est entre Min et Max
+prix_vin_min_max(Vin,Min,Max) :-
       prix(Vin,P),
       Min =< P, P =< Max.
 
+/***************************************************************************/
+% trouve un plat dans une liste de mots
+% format :
+%        get_plat(L_mots, Plat)
+%        in : L_mots est une liste de mots
+%        out : Plat est le nom d'un plat qui est un élément de ListeMots
 
-
-
-% Trouve un plat dans une liste de mots
 get_plat(L_mots, Plat) :-
   sublist(Plat, L_mots),
   accompagnement(_, Plat).
 
-% Trouve une liste L de vins pouvant accompagner un plat
+/***************************************************************************/
+% récupère une liste des vins pouvant accompagnez un plat donné
+% format :
+%        get_vin_acc(Plat, ListeIDvins)
+%        in : Plat est le nom d'un plat
+%        out : ListeIDvins est la liste des identifiants de vins pouvant
+%              accompagner Plat
+
 get_vin_acc(Plat,  L) :-
      findall( ID, accompagnement(ID, Plat), L).
 
-% Trouve une liste L d'id de vins pouvant accompagner un plat.
+/***************************************************************************/
+% récupère une liste de plats pouvant accompagnez un vin donné
+% format :
+%        get_plats(IDVin, ListePlats)
+%        in : IDVin est l'identifiant d'un vin
+%        out : ListePlats est la liste des plats pouvant accompagner IDVin
+
 get_plats(ID,  L) :-
      findall( Plat, accompagnement(ID, Plat), L).
